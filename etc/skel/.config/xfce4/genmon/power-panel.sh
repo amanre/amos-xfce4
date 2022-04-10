594462feb07acb85171c9042f4d787352bfc58ac
amanre <amanre556@gmail.com>
2022-04-10 17:06:27 +0200

    update

--- etc/skel/.config/xfce4/genmon/power-panel.sh
+++ etc/skel/.config/xfce4/genmon/power-panel.sh
@@ -8,16 +8,15 @@ readonly DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Insert the absolute path of the icon
 # Recommended size is 24x24 px
 readonly ICON="${DIR}/icons/power/power.png"
-
 # Uncomment the below and comment out the above if you prefer the apple logo instead
 #readonly ICON="${DIR}/icons/power/apple.png"
 
 # Panel
 if [[ $(file -b "${ICON}") =~ PNG|SVG ]]; then
   INFO="<img>${ICON}</img>"
-  INFO+="<click>arcolinux-logout</click>"
+  INFO+="<click>xfce4-session-logout</click>"
 else
-  INFO+="<click>arcolinux-logout</click>"
+  INFO+="<click>xfce4-session-logout</click>"
 fi
 
 # Tooltip
